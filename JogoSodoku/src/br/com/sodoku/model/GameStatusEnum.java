a477daedeabf520740df05d4d2134392402a9b5b
Aylla Scaglia <aylla@scaglia.com.br>
2025-02-14 13:25:33 -0300

    jogo sudoku

---
+++ JogoSodoku/src/br/com/sodoku/model/GameStatusEnum.java
@@ -0,0 +1,17 @@
+package br.com.sodoku.model;
+
+public enum GameStatusEnum {
+    NON_STARTED("não iniciado"),
+    INCOMPLETE("incompleto"),
+    COMPLETE("completo");
+
+    private String label;
+
+    GameStatusEnum(final String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
